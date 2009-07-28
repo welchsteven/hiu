fd7c37df1e7ede8814e3bbd7166ec9984f942a35
welchsteven <welchsteven@87e21136-64be-11de-b47b-210fc227597e>
2009-07-28 22:12:21 +0000

    Initial Import

---
+++ trunk/ARM/main.h
@@ -0,0 +1,29 @@
+/*
+ * main.h
+ *
+ *  Created on: Jul 22, 2009
+ *      Author: steven
+ */
+
+
+//#ifndef MAIN_H_
+//#define MAIN_H_
+
+
+//#endif /* MAIN_H_ */
+
+
+
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BAUDRATE B57600
+#define PORT "/dev/ttyUSB0"
+
+//int init_serial(int &fd);
+
+
